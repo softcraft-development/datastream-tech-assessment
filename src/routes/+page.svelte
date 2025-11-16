ed17308d44aa98bae8871c5f86a50e4cb14ef965
Craig Walker <github@softcraft.ca>
2025-11-15 19:54:21 -0700

    basic page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>DataStream Technical Assessment</h1>
+<h2>Craig Walker, November 2025</h2>
--- src/routes/page.svelte.spec.ts
+++ src/routes/page.svelte.spec.ts
@@ -1,13 +1,17 @@
-import { page } from "vitest/browser"
-import { describe, expect, it } from "vitest"
+import { beforeEach, describe, expect, it } from "vitest"
 import { render } from "vitest-browser-svelte"
+import { page } from "vitest/browser"
 import Page from "./+page.svelte"
 
 describe("/+page.svelte", () => {
-  it("should render h1", async () => {
-    render(Page)
+  describe("when it renders", () => {
+    beforeEach(() => {
+      render(Page)
+    })
 
-    const heading = page.getByRole("heading", { level: 1 })
-    await expect.element(heading).toBeInTheDocument()
+    it("should display the heading", async () => {
+      const heading = page.getByRole("heading", { level: 1 })
+      expect(heading.element()).toBeVisible()
+    })
   })
 })
